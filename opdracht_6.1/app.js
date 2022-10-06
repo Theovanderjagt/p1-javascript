d7720fab9e78d8c4b61e706784ceb6597c0d02c4
Theovanderjagt <9019593@student.zadkine.nl>
2022-10-06 13:12:54 +0200

    6_1

---
+++ opdracht_6.1/app.js
@@ -0,0 +1,6 @@
+function kleeur_aanpassen() {
+    document.getElementById("h1").style.color = "red";
+    document.getElementById("h1").innerText = "Ik ren nog steeds snel"
+}
+
+kleeur_aanpassen();
