eaaf34ee60ec59ce298c71786039eae3f6ea7df4
Theovanderjagt <9019593@student.zadkine.nl>
2022-10-07 09:45:25 +0200

    6_4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,18 @@
+function calculate(x, y) {
+let answer = x * y;
+console.log(answer)
+}
+
+calculate(5, 7);
+
+function calculate2 (x, y)
+let answer = x + y;
+console.log(answer)
+
+calculate (1, 12);
+
+function calculate3(x) {
+    console.log(x / 60)
+    }
+    
+    calculate3(3600);
