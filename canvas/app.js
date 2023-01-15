e19440e8c76edef2a488fdd6532f2b6c39f71a15
Theovanderjagt <112871814+Theovanderjagt@users.noreply.github.com>
2023-01-15 19:01:28 +0100

    6_1

---
+++ canvas/app.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", function (){
+const canvas = document.querrySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+})
+let painting = false;
+
+function startPosition(){
+    painting = true
+    console.log("im painting")
+    draw(e);
+};
+
+function finishedPosition(){
+    painting = false;
+    ctx.beginPath()
+    console.log("im done painting")
+};
+
+canvas.addEventListener("mousedown", startPosition);
+canvas.addEventListener("mouseup", finishedPosition);
+
+function draw() {
+    };
+canvas.addEventListener("mousemove", draw);
+
+function draw(e) {
+   if (!painting) return;
+    console.log(e);
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    ctx.lineTo(e.clientX, e.clientY);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(e.clientX, e.clientY)
+};
+
+
